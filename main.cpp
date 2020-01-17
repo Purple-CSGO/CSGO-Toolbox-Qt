1f8712203fb70d257fe219c59b5900447dabb2f1
Purple-CSGO <438518244@qq.com>
2020-01-17 10:59:03 +0800

    test title changing

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 
 #include <QApplication>
 
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowTitle("CSGO Toolbox");
     w.show();
     return a.exec();
 }
